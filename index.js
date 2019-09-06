8a557c08fafd02d04a650458aee2a65e774640f5
Beniamin Szantar <beniamin_szantar@gkpge.pl>
2019-09-06 12:41:11 +0200

    Added missing dependencies

--- index.js
+++
@@ -1,6 +0,0 @@
-export {default as useExcelExport} from './lib/useExcelExport';
-export {default as useSPDataFetch} from './lib/useSPDataFetch';
-export {default as useSPDictionaryFetch} from './lib/useSPDictionaryFetch';
-export {default as useSPListSearch} from './lib/useSPListSearch';
-export {default as useSPPeopleFetch} from './lib/useSPPeopleFetch';
-export {SecurityProvider, usePermissionCheck, useCurrentUser} from './lib/SecurityContext';
