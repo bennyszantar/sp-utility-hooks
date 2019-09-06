22f4a52bbf4aed21932f72b8d32a675d2bbf48a1
Beniamin Szantar <beniamin_szantar@gkpge.pl>
2019-09-06 08:11:10 +0200

    -mAdded Security Context and hooks for checking user permissions

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export {default as useSPDataFetch} from './lib/useSPDataFetch';
 export {default as useSPDictionaryFetch} from './lib/useSPDictionaryFetch';
 export {default as useSPListSearch} from './lib/useSPListSearch';
 export {default as useSPPeopleFetch} from './lib/useSPPeopleFetch';
+export {SecurityContext, usePermissionCheck, useCurrentUser} from './lib/SecurityContext';
